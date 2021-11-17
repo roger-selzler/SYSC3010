c9c512e4c7ba793920a2066ca46483a9a445eb81
roger-selzler <roger.selzler@carleton.ca>
2021-11-17 10:49:21 -0500

    activate/deactivate showip

---
+++ showip.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+activateshowip=false
+while getopts y: flag
+do
+  case "${flag}" in
+    y) activateshowip=true
+  esac
+done
+
+showipfile="/opt/sysc3010/showip"
+if activateshowip
+then
+  touch ${showipfile}
+else
+  [ -f ${showipfile} ] && sudo rm -rf "/opt/sysc3010/showip"
+fi
