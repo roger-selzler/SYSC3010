177f5cfe6d498270b99cb649d5a75a07680b4ec9
roger-selzler <roger.selzler@carleton.ca>
2021-11-17 18:55:21 -0500

    lab1 files

--- LAB1/send.py
+++ LAB1/send.py
@@ -18,8 +18,6 @@ while True:
                humidity=sense.get_humidity())
     MESSAGE = json.dumps(data).encode()
     sense.show_message(f"T: {data['temperature']:.2f}, H: {data['humidity']:.2f}")
-
-
     _log.debug(f"Sending message to {UDP_IP}:{UDP_PORT}: {MESSAGE}")
     sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
     time.sleep(5)
