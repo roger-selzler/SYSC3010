cbda03d8e31ae029b0afd7286fe2d868aca35b07
roger-selzler <roger.selzler@carleton.ca>
2021-11-17 11:14:05 -0500

    fix gitpull.sh script

--- showip.sh
+++ showip.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 activateshowip=false
-while getopts y: flag
-do
-  case "${flag}" in
-    y) activateshowip=true
-  esac
+#while getopts y: flag
+#do
+#  case "${flag}" in
+#    y) activateshowip=true
+#  esac
+#done
+for i in "$@";do
+  if [[$i==y]]; then
+    activateshowip=true
+  fi
 done
+
 
 showipfile="/opt/sysc3010/showip"
 if activateshowip
