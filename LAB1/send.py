cbb9f06496c5336f60c87ac8b5d403b65b0d9ca7
roger-selzler <roger.selzler@carleton.ca>
2021-11-17 18:26:27 -0500

    lab1 files

--- LAB1/send.py
+++ LAB1/send.py
@@ -5,7 +5,7 @@ import socket
 import json
 import time
 
-UDP_IP = "192.168.0.56"
+UDP_IP = "192.168.0.28"
 UDP_PORT = 6874
 
 _log.debug("UDP target IP: %s" % UDP_IP)
@@ -14,9 +14,12 @@ _log.debug("UDP target port: %s" % UDP_PORT)
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 while True:
-    MESSAGE = json.dumps(
-        dict(temperature=sense.get_temperature(),
-             humidity=sense.get_humidity()) ).encode()
-    _log.debug("Sending message: %s" % MESSAGE)
+    data = dict(temperature=sense.get_temperature(),
+               humidity=sense.get_humidity())
+    MESSAGE = json.dumps(data).encode()
+    sense.show_message(f"T: {data['temperature']:.2f}, H: {data['humidity']:.2f}")
+
+
+    _log.debug(f"Sending message to {UDP_IP}:{UDP_PORT}: {MESSAGE}")
     sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
     time.sleep(5)
